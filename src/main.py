c172d11d05230b58d878c7b103c2e0127c64d745
Daniman30 <regulardani1@gmail.com>
2024-07-02 23:32:46 -0400

    AST_printer added

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 from grammar.grammar import *
 from Parser.LR1_Parser import *
 from cmp.evaluation import *
+from semantic.AST_printer import *
 
 
 
@@ -13,4 +14,6 @@ print (tokens)
 parser = LR1Parser(G)
 derivation, operations = parser ([token.token_type for token in tokens ], get_shift_reduce=True)
 ast = evaluate_reverse_parse(derivation,operations,tokens)
+formatter = FormatVisitor()
+print(formatter.visit(ast))
 
